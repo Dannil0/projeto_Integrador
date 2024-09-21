96b1dc9d2e0f4b2cc11f5162f573e4337f70fbf7
Dannilo da Silva Sampaio <133255271+Dannil0@users.noreply.github.com>
2024-09-21 17:41:07 -0300

    cadastro
    java do cadastro

--- projeto_integrador/ProjInt/ProjInt/js/scr.js
+++ projeto_integrador/ProjInt/ProjInt/js/scr.js
@@ -1,1 +1,11 @@
-console.log("oi")
+function recebeDadoDeLogin() {
+    // Obtém todos os elementos com a classe 'boxLog'
+    let inputs = document.querySelectorAll('.boxLog');
+    
+    // Pega os valores dos inputs
+    let email = inputs[0].value;
+    let senha = inputs[1].value;
+
+    //retorna os dados adicionados no input
+    console.log(`usuario: ${email} senha: ${senha}`);
+}
