490a7ebafa14e21e9e1ffca81b5449c078f5dd03
Dannilo da Silva Sampaio <133255271+Dannil0@users.noreply.github.com>
2024-09-21 17:47:58 -0300

    socorro.help
    funcionou

--- projeto_integrador/ProjInt/ProjInt/js/scr.js
+++ projeto_integrador/ProjInt/ProjInt/js/scr.js
@@ -1,11 +1,24 @@
 function recebeDadoDeLogin() {
     // Obtém todos os elementos com a classe 'boxLog'
     let inputs = document.querySelectorAll('.boxLog');
-    
+
     // Pega os valores dos inputs
     let email = inputs[0].value;
     let senha = inputs[1].value;
 
     //retorna os dados adicionados no input
     console.log(`usuario: ${email} senha: ${senha}`);
+
+    // Define os valores esperados
+    const emailEsperado = "funfa";
+    const senhaEsperada = "prf";
+
+    // Verifica se os valores são os esperados
+    if (email === emailEsperado && senha === senhaEsperada) {
+        // Redireciona para a nova página desejada
+        window.location.href = "feed.html"; // Substitua "nova_pagina.html" pelo caminho da nova página
+    } else {
+        // Exibe uma mensagem de erro
+        alert("Usuário ou senha incorretos!");
+    }
 }
